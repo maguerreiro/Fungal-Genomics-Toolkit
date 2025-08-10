cc01b95b466a71ed18a84f4238297234ce794334
maguerreiro <128613688+maguerreiro@users.noreply.github.com>
2025-08-10 17:33:14 +0200

    update

--- utils/check_inputs.sh
+++ utils/check_inputs.sh
@@ -38,7 +38,8 @@
 
 
 check_inputs() {
-  local all_ok=1
+  local all_genomes_ok=1
+  local all_modules_ok=1
   local found_genomes=()
   local missing_genomes=()
   local found_modules=()
@@ -54,7 +55,7 @@ check_inputs() {
     else
       echo "[MISSING] $genome_path"
       missing_genomes+=("$genome")
-      all_ok=0
+      all_genomes_ok=0
     fi
   done
 
@@ -69,7 +70,7 @@ check_inputs() {
       else
         echo "[MISSING or NOT EXECUTABLE] $mod_path"
         missing_modules+=("$mod")
-        all_ok=0
+        all_modules_ok=0
       fi
     fi
   done
@@ -115,106 +116,24 @@ check_inputs() {
   done
 
  echo ""
-  if [ "$all_ok" -ne 1 ]; then
-    echo "ERROR: One or more required files or scripts are missing or not executable."
+  if [ "$all_genomes_ok" -ne 1 ]; then
+    echo "ERROR: One or more genomes are missing."
+    echo "Missing genome files:"
+      for g in "${missing_genomes[@]}"; do
+    echo "  $g"
+  done 
+    exit 1
+  fi
+
+   echo ""
+  if [ "$all_modules_ok" -ne 1 ]; then
+    echo "ERROR: One or more scripts are missing or not executable."
+      echo "Missing or non-executable modules:"
+  for m in "${missing_modules[@]}"; do
+    echo "  ${m}.sh"
+  done
     exit 1
   fi
 
   exit 0
 }
-
-
-
-
-
-# #!/bin/bash
-# # check_inputs.sh - input and module validation function
-
-# check_inputs() {
-#   local all_ok=1
-#   local found_genomes=()
-#   local missing_genomes=()
-#   local found_modules=()
-#   local missing_modules=()
-
-#   echo ""
-#   echo "Checking genome files in: $GENOME_DIR"
-#   for genome in "${GENOMES[@]}"; do
-#     genome_file="${genome##*/}"         # Strip path (if any)
-#     base_name="${genome_file%.*}"       # Remove extension
-#     genome_path="${GENOME_DIR}/${base_name}.fna"  # Expected extension .fna
-
-#     if [ -f "$genome_path" ]; then
-#       echo "[OK] $genome_path"
-#       found_genomes+=("$base_name")
-#     else
-#       echo "[MISSING] $genome_path"
-#       missing_genomes+=("$base_name")
-#       all_ok=0
-#     fi
-#   done
-
-#   echo ""
-#   echo "Checking module scripts in ./modules/"
-#   for mod in "${!MODULES_MAP[@]}"; do
-#     if [ "${MODULES_MAP[$mod]}" == "Yes" ]; then
-#       mod_path="./modules/${mod}.sh"
-#       if [ -x "$mod_path" ]; then
-#         echo "[OK] $mod_path"
-#         found_modules+=("$mod")
-#       else
-#         echo "[MISSING or NOT EXECUTABLE] $mod_path"
-#         missing_modules+=("$mod")
-#         all_ok=0
-#       fi
-#     fi
-#   done
-
-#   echo ""
-#   echo "===== SUMMARY ====="
-#   echo "Selected modules:"
-#   for mod in "${!MODULES_MAP[@]}"; do
-#     if [ "${MODULES_MAP[$mod]}" == "Yes" ]; then
-#       echo "  ${mod}.sh"
-#     fi
-#   done
-
-#   echo ""
-#   echo "Selected genomes:"
-#   for genome in "${GENOMES[@]}"; do
-#     echo "  $genome"
-#   done
-#   echo "==================="
-
-#   echo ""
-#   echo "Found genome files:"
-#   for g in "${found_genomes[@]}"; do
-#     echo "  $g"
-#   done
-
-#   echo ""
-#   echo "Missing genome files:"
-#   for g in "${missing_genomes[@]}"; do
-#     echo "  $g"
-#   done
-
-#   echo ""
-#   echo "Found modules:"
-#   for m in "${found_modules[@]}"; do
-#     echo "  ${m}.sh"
-#   done
-
-#   echo ""
-#   echo "Missing or non-executable modules:"
-#   for m in "${missing_modules[@]}"; do
-#     echo "  ${m}.sh"
-#   done
-
-#   echo ""
-#   if [ "$all_ok" -ne 1 ]; then
-#     echo "ERROR: One or more required files or scripts are missing or not executable."
-#     exit 1
-#   fi
-
-#   exit 0
-# }
